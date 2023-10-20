05d6ae6bb63d55d19817bde5e3f7195629b3c2fc
AaronYang <77483491+AaronYang1223@users.noreply.github.com>
2023-10-20 01:38:26 -0700

    update movement info func

--- key_joy/src/key_joy_node.py
+++ key_joy/src/key_joy_node.py
@@ -38,42 +38,75 @@ class KeyJoyNode:
             if n <= len(lines):
                 x, y, z = map(float, lines[n-1].split(','))  # Indexing starts from 0
                 return x, y, z
-            
-    def get_target_angle(self, current, target):
-        delta_x = target[0] - current[0]
-        delta_y = target[1] - current[1]
-        return math.atan2(delta_y, delta_x)
-    
-    def decide_movement_order_with_tolerance(self, coordinates, tolerance=0.01):
-        actions = []
-        for i in range(len(coordinates) - 1):
-            current_x, current_y, current_z = coordinates[i]
-            target_x, target_y, _ = coordinates[i+1]
-            
-            z_1 = self.get_target_angle((current_x, current_y), (target_x, target_y))
-            angle_difference = z_1 - current_z
-            
-            # Normalize the difference to between -pi and pi
-            while angle_difference > math.pi:
-                angle_difference -= 2 * math.pi
-            while angle_difference < -math.pi:
-                angle_difference += 2 * math.pi
-            
-            # Check if the difference is within the acceptable values or within the tolerance
-            if abs(angle_difference) < tolerance or round(angle_difference % 3.14, 2) in [0, 3.14, 1.57, -1.57]:
-                closest_angle = min([0, 3.14, 1.57, -1.57], key=lambda x: abs(x-angle_difference))
-                actions.append(['move', closest_angle])
-            else:
-                # Compare with 0 and 3.14 to decide the rotation angle
-                if abs(angle_difference) < abs(angle_difference - math.pi):
-                    actions.append(['rotate', angle_difference])
-                else:
-                    actions.append(['rotate', angle_difference - math.pi])
-                
-        return actions
-    
+######
     def calculate_distance(self, point1, point2):
+        """
+        Calculates the Euclidean distance between two 2D points.
+        """
         return math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+
+    def determine_angle_details(self, points):
+        """
+        Determines the target angle and the angle difference for the robot for each pair of points.
+        """
+        angle_details = []
+        for i in range(len(points) - 1):
+            point1, point2 = points[i], points[i+1]
+            # Calculate target angle
+            z_target_axis = math.atan2(point2[1] - point1[1], point2[0] - point1[0])
+            # Normalize the result to between -pi and pi
+            while z_target_axis > math.pi:
+                z_target_axis -= 2 * math.pi
+            while z_target_axis < -math.pi:
+                z_target_axis += 2 * math.pi
+            # Calculate angle difference
+            angle_difference_robot_target = z_target_axis - point1[2]
+            # Normalize the result to between -pi and pi
+            while angle_difference_robot_target > math.pi:
+                angle_difference_robot_target -= 2 * math.pi
+            while angle_difference_robot_target < -math.pi:
+                angle_difference_robot_target += 2 * math.pi
+            # Append the details to the list
+            angle_details.append({"point1": point1, "point2": point2, "z_target_axis": z_target_axis, "angle_difference_robot_target": angle_difference_robot_target})
+        return angle_details
+
+    def detailed_movement_information_simplified(self, angle_details):
+        """
+        Provides detailed movement information for both 'rotate' and 'move' actions, with simplified calculations.
+        Returns a list of actions and their respective details.
+        """
+        results = []
+        for detail in angle_details:
+            angle_diff = round(detail["angle_difference_robot_target"], 2)
+            # Check if the rounded angle difference is close to any of the four angles for 'move' actions
+            if any(abs(angle_diff - val) <= 0.05 for val in [0, 3.14, 1.57, -1.57]):
+                distance = self.calculate_distance((detail["point1"][0], detail["point1"][1]), (detail["point2"][0], detail["point2"][1]))
+                rotation_after_move = detail["point2"][2] - detail["point1"][2]
+                while rotation_after_move > math.pi:
+                    rotation_after_move -= 2 * math.pi
+                while rotation_after_move < -math.pi:
+                    rotation_after_move += 2 * math.pi
+                results.append(['move', angle_diff, distance, rotation_after_move])
+            # For 'rotate' actions
+            else:
+                if angle_diff < 1.57:
+                    rotation_angle_change_1 = angle_diff
+                    angle_difference_robot_target_1 = 0
+                else:
+                    rotation_angle_change_1 = 3.14 - angle_diff
+                    angle_difference_robot_target_1 = 3.14
+                distance = self.calculate_distance((detail["point1"][0], detail["point1"][1]), (detail["point2"][0], detail["point2"][1]))
+                # Calculate the rotation angle required after reaching point2
+                rotation_angle_change_2 = detail["point2"][2] - (detail["point1"][2] + rotation_angle_change_1)
+                # Normalize rotation_angle_change_2 to between -pi and pi
+                while rotation_angle_change_2 > math.pi:
+                    rotation_angle_change_2 -= 2 * math.pi
+                while rotation_angle_change_2 < -math.pi:
+                    rotation_angle_change_2 += 2 * math.pi
+                results.append(['rotate', rotation_angle_change_1, angle_difference_robot_target_1, distance, rotation_angle_change_2])
+        return results
+
+#####
 
     def run(self):
         # while True:
@@ -104,20 +137,18 @@ class KeyJoyNode:
         print(all_coordinates)
         #
 
-        movement_order_with_tolerance = self.decide_movement_order_with_tolerance(all_coordinates)
+        angle_details = self.determine_angle_details(all_coordinates)
 
         #
-        print(movement_order_with_tolerance)
+        print(angle_details)
         #
 
-        distances = []
-        for i in range(len(all_coordinates) - 1):
-            current_x, current_y, _ = all_coordinates[i]
-            target_x, target_y, _ = all_coordinates[i+1]
-            distance = self.calculate_distance((current_x, current_y),(target_x, target_y))
-            distances.append(distance)
-        
-        print(distances)
+        self.detailed_move_rotate_info = self.detailed_movement_information_simplified(angle_details)
+
+        #
+        print(angle_details)
+        #
+
         self.stop()
 
     # constant
