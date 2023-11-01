d424fd32fbf84057f98e2c2616f194732e8db92d
YifanWang00 <yifanwang.xyz@gmail.com>
2023-11-01 06:46:32 +0000

    Update
    Signed-off-by: YifanWang00 <yifanwang.xyz@gmail.com>

--- map_to_markers_tf.sh
+++ map_to_markers_tf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rosrun tf static_transform_publisher 1.625 0 0 0 0 0 map marker_0 &
-rosrun tf static_transform_publisher 1.625 2 0 0 0 0 map marker_1 &
-rosrun tf static_transform_publisher -0.625 2.5 0 0 0 0 map marker_2 &
-rosrun tf static_transform_publisher -0.625 1 0 0 0 0 map marker_3 &
-rosrun tf static_transform_publisher -0.1875 -0.75 0 0 0 0 map marker_4 &
+rosrun tf static_transform_publisher 1.625 0 0 0 0 0 0 map marker_0 &
+rosrun tf static_transform_publisher 1.625 2 0 0 0 0 0 map marker_1 &
+rosrun tf static_transform_publisher -0.625 2.5 0 0 0 0 0 map marker_2 &
+rosrun tf static_transform_publisher -0.625 1 0 0 0 0 0 map marker_3 &
+rosrun tf static_transform_publisher -0.1875 -0.75 0 0 0 0 0 map marker_4 &
